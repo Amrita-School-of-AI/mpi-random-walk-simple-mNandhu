46bc426a75e36bb62d8894e3d30b1ccc8770c2fd
Kiruthik Nandhan Murthi <38995539+mNandhu@users.noreply.github.com>
2025-08-07 21:14:00 +0530

    fix: Add missing semicolon
    - :(

--- random_walk.cpp
+++ random_walk.cpp
@@ -70,7 +70,7 @@ void walker_process()
     for (int step = 0; step < max_steps; step++)
     {
         int move = (rand() % 2 == 0) ? -1 : 1;
-        walker_pos += move
+        walker_pos += move;
         if (walker_pos < -domain_size || walker_pos > domain_size || i == max_steps -1))
         {
             std::cout << "Rank " << world_rank << ": Walker finished in" << step + 1 << " steps." << std::endl;
